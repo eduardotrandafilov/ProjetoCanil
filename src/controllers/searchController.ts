ffe629f2c3e81daea98ed2d7dacd9272a8aae9b9
eduardotrandafilov <eduardo.trandafilov@gmail.com>
2021-11-05 08:36:38 -0300

    Modificacao de dados da pagina e tratamento ativacao do menu por tipo de animal

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,12 @@
 import { Request,Response} from "express";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const search = (req:Request,res:Response)=>{
-    res.render("search");
+    res.render("pages/page",{
+        menu:createMenuObject(""),
+        banner:{
+            title:"Todos os animais",
+            background:"allanimals.jpg"
+        }
+    });
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,9 @@ import * as SearchController from "../controllers/searchController";
 const router = Router();
 
 router.get("/",PageController.home);
-router.get("/dogs",PageController.dog);
-router.get("/fish",PageController.fish);
+router.get("/dogs",PageController.dogs);
+router.get("/cats",PageController.cats);
+router.get("/fishes",PageController.fishes);
 router.get("/search",SearchController.search);
 
 export default router;
