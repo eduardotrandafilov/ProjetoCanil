e303ae8da1491dfeea4e55e0852e9c147fc2bd1e
eduardotrandafilov <eduardo.trandafilov@gmail.com>
2021-11-06 09:26:21 -0300

    Criacao model e listagem dos pets nos controllers

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,43 +1,52 @@
 import { Request,Response} from "express";
 import { createMenuObject } from "../helpers/createMenuObject";
+import {PetObject} from "../models/Pet";
 
 export const home = (req:Request,res:Response)=>{
+    const lista = PetObject.getAll();
     res.render("pages/page",{
         menu:createMenuObject("all"),
         banner:{
             title:"Todos os animais",
             background:"allanimals.jpg"
-        }
+        },
+        lista
     });
 }
 
 export const dogs = (req:Request,res:Response)=>{
+    const lista = PetObject.getFromType("dog");
     res.render("pages/page",{
         menu:createMenuObject("dog"),
         banner:{
             title:"Cachorros",
             background:"banner_dog.jpg"
-        }
+        },
+        lista
     });
 }
 
 export const cats = (req:Request,res:Response)=>{
+    const lista = PetObject.getFromType("cat");
     res.render("pages/page",{
         menu:createMenuObject("cat"),
         banner:{
             title:"Gatos",
             background:"banner_cat.jpg"
-        }
+        },
+        lista
     });
 }
 
 
 export const fishes = (req:Request,res:Response)=>{
+    const lista = PetObject.getFromType("fish");
     res.render("pages/page",{
         menu:createMenuObject("fish"),
         banner:{
             title:"Peixes",
             background:"banner_fish.jpg"
-        }
+        },
+        lista
     });
 }
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,12 +1,17 @@
 import { Request,Response} from "express";
+import { convertToObject } from "typescript";
 import { createMenuObject } from "../helpers/createMenuObject";
+import { PetObject } from "../models/Pet";
 
 export const search = (req:Request,res:Response)=>{
+    const nome = req.query.q;
+    const lista = PetObject.getFromName(nome);
     res.render("pages/page",{
         menu:createMenuObject(""),
         banner:{
             title:"Todos os animais",
             background:"allanimals.jpg"
-        }
+        },
+        lista
     });
 }
